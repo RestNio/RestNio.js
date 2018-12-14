07e9d7da2d60c4c0716549692dd1152b84f4635e
7kasper <kasper77077@gmail.com>
2018-12-14 23:38:35 +0100

    More advanced custom checking + begin standard lib.

--- test/dogSite/dogs/tekkel/index.js
+++ test/dogSite/dogs/tekkel/index.js
@@ -1,36 +1,37 @@
+const RNIO = require('../../../../index');
+const $f = RNIO.params.formatters;
+const $c = RNIO.params.checks;
+
 module.exports = (router) => {
-    router.all('/schop', (params, client) => {
-        // TODO: Parsing boolean params properly.
+
+    router.all('/kick', (params, client) => {
         if (params.hard) {
             return "AAAAAAUUUWUUUWWWWWWUWUUW!!!!";
-            // runLater(getFromDatabase, (result) => {
-            //     client.end(JSON.stringify(result));
-            // });
-            // return Infinity;
-            // return new Promise((resolve, reject) => {
-            //      let jan = getFromDB();
-            //      resolve(jan);   
-            // }
         } else {
             return "AUW WOOF!";
         }
-    },
-    { 
+    }, { 
         hard: {
             required: true, 
             type: 'boolean',
             checks: [
-                (value, reject) => {
-                    if (value) {
-                        reject('You may not kick the dog in such a way!');
+                (value, name, reject) => {
+                    if (value === true) {
+                        reject('You may not kick the dog in such a ' + name + ' a way!');
                     }
                 }
             ]
         }
     },
     ['dogs.tekkel.schop.$hard', 'animals.bystander']);
-    // router.get('/give', (params) => {
 
-    // });
+    router.get('/cuddle', (params) => {
+        return "Cuddled tekkel " + params.times + " times!";
+    }, {times: {required: true, type: 'number', prechecks: [$c.num.min(1)], formatters: [$f.num.add(2)], checks: [$c.num.max(5)]}});
+
+    router.get('/feed', (params, client) => {
+        client.json({eaten: true});
+    });
+
     return router;
 }
