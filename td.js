cfe29c2556b5df75499a9e3eed611491cab6410e
7kasper <kasper77077@gmail.com>
2018-12-24 11:43:07 +0100

    Add comments to permission set. Add many methods.

--- td.js
+++ td.js
@@ -1,23 +1,26 @@
-let permission = 'test.dogs.tekkel.cuddle'; //required
-let holder = [
-    '*'
-    //'test.dogs.tekkel.*',
-    //'test.dogs.tekkel.*'
-];
-let permissionX = permission.split('.');
+console.log(require('./lib/util/jsUtils').isIterable(true));
 
-for(let perm of holder) {
-    let permX = perm.split('.');
-    for (let i = permX.length - 1; i >= 0; i--) {
-        if (permX[i] !== '*') {
-            if (permX[i] !== permissionX[i]) {
-                console.log('PERMISSION DENIED: ' + permX[i] + ' !== ' + permissionX[i]);
-                return;
-            }
-        }
-    }
-    console.log('PERMISSION GRANTED!')
-    return;
-};
+
+// let permission = 'test.dogs.tekkel.cuddle'; //required
+// let holder = [
+//     '*'
+//     //'test.dogs.tekkel.*',
+//     //'test.dogs.tekkel.*'
+// ];
+// let permissionX = permission.split('.');
+
+// for(let perm of holder) {
+//     let permX = perm.split('.');
+//     for (let i = permX.length - 1; i >= 0; i--) {
+//         if (permX[i] !== '*') {
+//             if (permX[i] !== permissionX[i]) {
+//                 console.log('PERMISSION DENIED: ' + permX[i] + ' !== ' + permissionX[i]);
+//                 return;
+//             }
+//         }
+//     }
+//     console.log('PERMISSION GRANTED!')
+//     return;
+// };
 
 
