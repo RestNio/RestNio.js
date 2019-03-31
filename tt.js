e5f00709ba94a9c3f8f64a4b55109e9dac783d88
7kasper <kasper77077@gmail.com>
2019-03-31 17:39:54 +0200

    Work on regex maps.

--- tt.js
+++ tt.js
@@ -5,9 +5,20 @@ let dogSite = new RestNio((router, restnio) => {
     //     return [...restnio.routes];
     // });
 
-    router.all('/$name/hi', () => {
-        return 'Lolalalala';
+    router.all('/$name/hi', (params) => {
+        return `${params.name} is een aardig persoon.`;
     });
+
+    router.get('/day/maandag', () => 'Dat is een leuke dag');
+    router.get('/location/*/give', () => 'nu gaat ie mis');
+
+    router.use('/derp', (router) => {
+        router.get('/', () => 'derpindex');
+        router.get('/name', () => 'kasper');
+    }, true);
+
+    router.redirect('/test', '/derp/name');
+
     // router.use(restnio.serve('./README.md'));
 }, {port: 7070});
 console.dir(dogSite.routes);
