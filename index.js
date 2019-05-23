b3e8b2114683a9d13eb95d4992e62abd7cc0ed8c
7kasper <kasper77077@gmail.com>
2019-05-23 17:14:35 +0200

    Began work on cookies and improving json

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ module.exports = require('./lib/RestNio');
 
 /**
  * @typedef {import("./lib/routes/Router").RouteBack} RouteBack
+ * @typedef {import("./lib/routes/Route")} Route
+ * @typedef {import("./lib/routes/Route").RouteFunc} RouteFunc
+ * @typedef {import("./lib/routes/Route").RouteDef} RouteDef
+ * @typedef {import("./lib/routes/Route").ParamDef} ParamDef
+ * @typedef {import("./lib/client/Client")} Client
+ * @typedef {import("./lib/params/").Params} Params
+ * @typedef {import("./lib/params/formatters/Num").NumFormat} NumFormat
+ * @typedef {import("./lib/params/formatters/Str").StrFormat} StrFormat
+ * @typedef {import("./lib/params/checks/Num").NumCheck} NumCheck
+ * @typedef {import("./lib/params/checks/Str").StrCheck} StrCheck
  */
