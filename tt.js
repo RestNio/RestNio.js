47216497bc6ac4161cae5c5d72c918f27f9c23cd
7kasper <kasper77077@gmail.com>
2020-07-29 22:30:17 +0200

    Fix routeMap. Added double star.

--- tt.js
+++ tt.js
@@ -6,27 +6,31 @@ let main = new restnio((router, rnio) => {
         return 'HIII :D';
     });
 
-    router.use('/ree/:dope/', rnio.ratelimit({
-        per: 'params', perParams: ['dope', 'dape']
-    }));
-    router.get('/ree/:dope', (params)=>params.dope);
-    router.get('/ree/:dope/dape', (params)=>params.dope + params.dape);
+    // router.use('/ree/:dope/', rnio.ratelimit({
+    //     per: 'params', perParams: ['dope', 'dape']
+    // }));
+    // router.get('/ree/:dope', (params)=>params.dope);
+    // router.get('/ree/:dope/dape', (params)=>params.dope + params.dape);
 
-    router.use('/whatismyip', rnio.ratelimit({
+    // router.get('/drep', () => 'ddd');
+    router.get('/drep**', () => 'eee');
+    // router.get('/drep/*/test', () => '111');
 
-    }));
-    router.get('/whatismyip', (params, client) => client.ip);
+    // router.use('/whatismyip', rnio.ratelimit({
 
-	//router.redirect('/docs', '/docs/');
-    router.use('/docs', rnio.serve('./docs/', {cache:false, doListing: true, noFilename:true}), true);
+    // }));
+    // router.get('/whatismyip', (params, client) => client.ip);
 
-    router.get('/cookie', (params, client) => {
+	// //router.redirect('/docs', '/docs/');
+    // router.use('/docs', rnio.serve('./docs/', {cache:false, doListing: true, noFilename:true}), true);
+
+    // router.get('/cookie', (params, client) => {
         
-    });
+    // });
 
-    router.post('/nomnom/:extraparam', (params, client) => {
-        return {headers: client.headers, cookies: client.cookies, params: params};
-    });
+    // router.post('/nomnom/:extraparam', (params, client) => {
+    //     return {headers: client.headers, cookies: client.cookies, params: params};
+    // });
 
 }, {
     port: 7070,
